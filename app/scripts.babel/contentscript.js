18ed178d9febd14a7adf1ad45ace862a58aafccb
Andrew Myers <maskedcoder@hush.com>
2017-10-25 11:56:20 -0500

    Add basic blocking functionality

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,1 +1,83 @@
-console.log('\'Allo \'Allo! Content script');
+function removePageBlock() {
+  const $els = Array.from(document.querySelectorAll('.js-page-block'));
+
+  $els.forEach(($el) => {
+    $el.parentElement.removeChild($el);
+  });
+}
+
+function showWarning(violations) {
+  document.body.className += ' page-block-scroll-lock';
+
+  const style = document.createElement('style');
+  style.className = 'js-page-block';
+
+  style.innerHTML = `
+.page-block-scroll-lock {
+  overflow: hidden !important;
+}
+
+.page-block {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow-y: scroll;
+  background: #fff;
+  z-index: 99999;
+}
+
+.page-block__content {
+  display: block;
+  max-width: 300px;
+  margin: 20vh auto;
+}
+`;
+
+  const $el = document.createElement('div');
+  $el.className = 'js-page-block page-block';
+
+  const html = `
+<div class="page-block__content">
+  <p><strong>Are you sure you want to continue?</strong></p>
+  <p>This page has the following violations:</p>
+  <ul>
+    ${violations}
+  </ul>
+  <button class="js-page-block-remove">Yes, I'm sure</button>
+</div>`;
+
+  $el.innerHTML = html;
+
+  document.head.appendChild(style);
+  document.body.appendChild($el);
+
+  document.body.querySelector('.js-page-block-remove').addEventListener('click', () => {
+    removePageBlock();
+  });
+}
+
+function checkSites(sites) {
+  const matches = sites.filter(site => (new RegExp(site)).test(location.host));
+
+  if (matches) {
+    const violations = Array.prototype.concat.apply([], matches.map((site) => {
+      const rules = site.rules.replace(/\n/g, ',');
+
+      return Array.from(document.querySelectorAll(rules));
+    })).map($el => $el.innerText);
+
+    if (violations.length) {
+      showWarning(violations);
+    }
+  }
+}
+
+function loadModel() {
+  chrome.storage.sync.get({
+    sites: [],
+  }, data => checkSites(data.sites));
+}
+
+loadModel();
--- app/scripts.babel/options.js
+++ app/scripts.babel/options.js
@@ -1,1 +1,169 @@
-console.log('\'Allo \'Allo! Option');
+/* global $ */
+
+// Saves options to chrome.storage.sync.
+
+const $ = (selector) => document.querySelector(selector);
+
+function createTableEntry(name) {
+  return `
+<td>${name}</td>
+<td>
+  <button class="js-row-edit-site">Edit</button>
+  <button class="js-delete-site">Remove</button>
+</td>`;
+}
+
+let MODEL = [];
+
+function getItem(id) {
+  return MODEL.find(item => item.id === id);
+}
+
+function deleteItem(id) {
+  MODEL = MODEL.filter(item => item.id !== id);
+}
+
+function editItem(id, key, value) {
+  getItem(id)[key] = value;
+}
+
+function generateTable(sites) {
+  const $table = $('.js-sites-table');
+
+  const html = sites
+    .map((site) => `<tr class="js-site" data-id="${site.id}">${createTableEntry(site.name)}</tr>`)
+    .join('');
+
+  $table.innerHTML = html;
+}
+
+function setModel(sites) {
+  MODEL = sites;
+  generateTable(sites);
+}
+
+function loadModel() {
+  chrome.storage.sync.get({
+    sites: [],
+  }, data => setModel(data.sites));
+}
+
+function saveModel() {
+  chrome.storage.sync.set({
+    sites: MODEL,
+  }, () => {
+    const $status = $('.js-status');
+
+    $status.textContent = 'Options saved.';
+
+    setTimeout(() => {
+      $status.textContent = '';
+    }, 750);
+  });
+}
+
+function getId($el) {
+  const parent = $el.parentElement.parentElement;
+
+  return Number(parent.dataset.id);
+}
+
+function deleteSite(id) {
+  deleteItem(id);
+  generateTable(MODEL);
+}
+
+function editSite(id) {
+  $('.js-add-site').hidden = true;
+  $('.js-add-site-input').hidden = true;
+  $('.js-edit-site').hidden = false;
+  $('.js-edit-site-input').hidden = false;
+
+  $('.js-edit-site-name').value = getItem(id).name;
+  $('.js-edit-site-name').dataset.id = id;
+}
+
+function editRules(id) {
+  const item = getItem(id);
+
+  $('.js-which-site').innerHTML = `Editing site <code>/${item.name}/</code>`;
+  $('.js-site-rules').value = item.rules;
+  $('.js-save-rules').dataset.id = id;
+}
+
+$('.js-add-site').addEventListener('click', () => {
+  const name = $('.js-new-site-name').value;
+
+  if (name) {
+    try {
+      RegExp(name);
+    } catch (e) {
+      return;
+    }
+
+    const item = {
+      name,
+      id: +new Date(),
+      rules: '',
+    };
+
+    MODEL.push(item);
+
+    const $el = document.createElement('tr');
+    $el.className = 'js-site';
+    $el.dataset.id = item.id;
+
+    $el.innerHTML = createTableEntry(item.name);
+
+    $('.js-sites-table').appendChild($el);
+
+    $('.js-new-site-name').value = '';
+  }
+});
+
+$('.js-edit-site').addEventListener('click', () => {
+  const name = $('.js-edit-site-name').value;
+
+  if (name) {
+    try {
+      RegExp(name);
+    } catch (e) {
+      return;
+    }
+
+    const id = Number($('.js-edit-site-name').dataset.id);
+
+    editItem(id, 'name', name);
+    generateTable(MODEL);
+
+    $('.js-add-site').hidden = false;
+    $('.js-add-site-input').hidden = false;
+    $('.js-edit-site').hidden = true;
+    $('.js-edit-site-input').hidden = true;
+  }
+});
+
+$('.js-save-rules').addEventListener('click', () => {
+  const rules = $('.js-site-rules').value;
+  const id = Number($('.js-save-rules').dataset.id);
+
+  editItem(id, 'rules', rules);
+});
+
+$('.js-save').addEventListener('click', () => saveModel());
+
+$('body').addEventListener('click', (e) => {
+  const list = e.target.classList;
+
+  if (list.contains('js-delete-site')) {
+    deleteSite(getId(e.target));
+  } else if (list.contains('js-row-edit-site')) {
+    editSite(getId(e.target));
+    editRules(getId(e.target));
+  } else if (e.target.parentElement &&
+             e.target.parentElement.classList.contains('js-site')) {
+    editRules(Number(e.target.parentElement.dataset.id));
+  }
+});
+
+loadModel();
